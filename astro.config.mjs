5a6f1d30b3ea68af83dadb6491f517bbdfaf18f5
jacobmonck <jacobamonck@gmail.com>
2025-12-16 22:43:46 -0500

    fix: remove sharp image optimization

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,11 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   site: 'https://monck.dev',
-
+  image: {
+    service: {
+      entrypoint: 'astro/assets/services/noop',
+    },
+  },
   vite: {
     plugins: [tailwindcss()],
   },
